8dc462923e036bd51b93af9043effd37b79dfe35
newmacuser <ytm1234@live.com>
2025-11-06 19:03:02 -0500

    Add files via upload

--- SCFAs_v2.R
+++ SCFAs_v2.R
@@ -7,17 +7,25 @@ library(tidyverse)
 table.A <- read.table("compounds.txt", fill = T, sep = "\t") %>%
   as_tibble() %>%
   select(V2, V6) %>%
-  filter(V2 != "") 
+  filter(V2 != "" & V6 != "NA") 
+table.A2 <- table.A %>%
+  mutate(run = cumsum(str_to_lower(V2) == "Acetate" & is.na(V6))) %>%
+  filter(run == 0) %>%
+  nrow()
 
-read.table("header.txt", fill = T, sep = "\t", row.names = NULL) %>%
-  as_tibble() %>%
-  filter(row.names == "Data File Name") %>%
-  select("X.Header.") %>%
-  slice(rep(1:n(), each = 14)) %>%
-  bind_cols(table.A) %>%
-  group_by(V2) %>%
+table.B <- read.table("header.txt", fill = T, sep = "\t", row.names = NULL) %>%
+  as_tibble()
+
+table.B2 <- table.B %>%
+  filter( .data[[ names(table.B)[1] ]] == "Data File Name") %>%
+  select(names(table.B)[2]) %>%
+  slice(rep(1:n(), each = paste(which(duplicated(table.A$V2))[1]-1) )) %>%
+  bind_cols(table.A) 
+
+table.B2 %>%
+  group_by(.data[[ names(table.B2)[2] ]]) %>%
   mutate(row = row_number()) %>%
-  pivot_wider(names_from = "V2", values_from = "V6") %>%
+  pivot_wider(names_from = names(table.B2)[2], values_from = names(table.B2)[3]) %>%
   select(-row) %>%
-  rename("SampleID" = "X.Header.") %>%
+  rename_with(~ "SampleID", .cols = 1) %>%
   write.csv("../out/output.concentration.csv", row.names = F)
